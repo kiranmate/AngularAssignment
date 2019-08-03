07d6f3d80d8c57a82aa66b482c37aa460cd99132
kiranmate <52972354+kiranmate@users.noreply.github.com>
2019-08-03 09:58:34 +0530

    Add files via upload
    first assignment (register handler)

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log(console);
+
+console.log(document);
+console.log(document.getElementById('tb_1'));
+document.getElementById('tb_1').style.border = '3px solid green';
+
+
+function PopDv(val, dv)
+{
+    dv.innerHTML=val;
+}
+
