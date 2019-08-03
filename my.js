738efe0ae608da44adbfb612ee69b7aa84a6b234
kiranmate <52972354+kiranmate@users.noreply.github.com>
2019-08-03 09:54:34 +0530

    javascript programs
    javascript program for math functions

---
+++ my.js
@@ -0,0 +1,27 @@
+let num1=50;
+let num2=20;
+
+console.log(`addition is ${num1+num2}`)
+console.log(`substraction is ${num1-num2}`)
+console.log(`Multiplication is ${num1*num2}`)
+console.log(`Division is ${num1/num2}`)
+
+var no=5;
+if(no%2==0)
+{
+console.log('Number is even')
+
+}
+else{
+    console.log('Number is odd')
+}
+
+var number=8;
+console.log(`Square of a number is ${number*number}`)
+
+var sq=81;
+console.log(`SquareRoot of a number is: ${Math.sqrt(sq)}`)
+
+var cb=6;
+
+console.log(`Cube of a number is: ${cb*cb*cb}`)
